a581705bc6316c31b1c0ebec88cb2100603d9c10
Mohamed Elfalaky <106435178+MohamedElfalaky@users.noreply.github.com>
2024-07-08 19:58:02 +0300

    day 2 flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:iti_summmer_7_quiz_app/notes.dart';
+import 'package:iti_summmer_7_quiz_app/screens/landing_screen.dart';
+import 'package:iti_summmer_7_quiz_app/screens/questions_screen.dart';
+import 'dart:io' show Platform;
 
 void main() {
   runApp(const MyApp());
@@ -16,75 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: TestScreen(),
-    );
-  }
-}
-
-class TestScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.logout),
-          onPressed: () {},
-        ),
-        actions: [
-          Icon(Icons.message),
-          SizedBox(
-            width: 12,
-          ),
-          Icon(
-            Icons.notification_add,
-          ),
-          SizedBox(
-            width: 12,
-          ),
-          Icon(Icons.search),
-          SizedBox(
-            width: 12,
-          )
-        ],
-        title: Text("Quiz app"),
-        // centerTitle: false,
-        backgroundColor: Colors.deepOrangeAccent,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(22),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                "1st Qestion",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "What is the last country to host the world cup?",
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(
-                height: 22,
-              ),
-              ElevatedButton(onPressed: () {}, child: Text("USA")),
-              ElevatedButton(onPressed: () {}, child: Text("Qatar")),
-              ElevatedButton(onPressed: () {}, child: Text("France")),
-              ElevatedButton(onPressed: () {}, child: Text("Egypt")),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: "Settings"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Person"),
-        ],
-      ),
+      home: LandingScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Center(
+        child: Text("Login screen"),
+      ),
+    );
+  }
+}
